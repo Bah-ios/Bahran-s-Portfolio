85039097b0ddfa9c1ea0331f8be76194448a853f
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-04-23 09:36:27 +0300

    Navbar sliding enterance & staged menu

--- my_portfolio/components/Navbar.tsx
+++ my_portfolio/components/Navbar.tsx
@@ -1,20 +1,41 @@
+"use client"
 import Link from 'next/link';
 import styles from './Navbar.module.css';
-
+import { motion } from "framer-motion";
 const Navbar = () => {
   const items = ['About','Skills', 'Projects', 'Contact'];
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.brand}>BS</div>
+    <motion.nav
+     className={styles.navbar}
+     initial= {{ y: -100, opacity : 0}}
+     animate= {{ y: 0, opacity : 1}}
+     transition={{ duration : 0.8, ease: "easeOut"}}
+     >
+      <motion.div className={styles.brand}
+      initial={{ scale: 0, rotate:-180 }}
+      animate={{ scale: 1, rotate: 0}}
+      transition={{ duration: 0.9, delay: 0.2}}
+      whileHover={{scale: 1.1, rotate: 5}}
+
+      >BS</motion.div>
       <div className={styles.links}>
-        {items.map((item) => (
-          <a key={item} href={`#${item.toLowerCase()}`} className={styles.link}>
+        {items.map((item, index) => (
+          <motion.a 
+          key={item} 
+          href={`#${item.toLowerCase()}`} 
+          className={styles.link}
+          initial={{ y: -20, opacity: 0}}
+          animate={{ y: 0, opacity: 1}}
+          transition={{ delay: 0.5 + ( index * 0.1)}}
+          whileHover={{ scale: 1.1, y: -2 }}
+          whileTap={{ scale: 0.95 }}
+          >
             {item}
-          </a>
+          </motion.a>
         ))}
         <a href="#contact" className={styles.hireBtn}>HIRE ME</a>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
